c4a93b84060eef64d5b36792a021839a2522bec4
uchenna <uchennaonwuliri@gmail.com>
2022-05-23 22:57:58 +0100

    solution

--- number of params/functionParameters.js
+++ number of params/functionParameters.js
@@ -1,9 +1,11 @@
 let f = function (a, b) {
-    
+    //length is the property of the function that tell you how many things have been defined as a function parameter 
+    arguments.length === f.length?console.log('we have a match'):console.log('No match')
     
 };
 
 //determine if the function received the number of params it expected
 f(1);
 f(1, 2);
-f(1, 2, 3);
+f(1, 2, 3); 
+// arguments refers to the number of parameters passed in 
