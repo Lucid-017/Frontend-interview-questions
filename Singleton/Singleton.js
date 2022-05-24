2d087a9d835d6f80759da85eb7b282c89f248a13
Lucid <67615874+Lucid-017@users.noreply.github.com>
2022-05-24 13:28:21 +0100

    Test case

--- Singleton/Singleton.js
+++ Singleton/Singleton.js
@@ -7,9 +7,20 @@
 let obj = (function () {
     let objInstance; //private variable
     function create() { //private function to create methods and properties
-        
+        let _isRunning =false
+        let start =()=>{
+            _isRunning = true
+        }
+        let stop =()=>{
+            _isRunning=false
+        }
+        let currentState =()=>{
+           return _isRunning
+        }
         return{//this return now represents the object inside objInstance
-
+            start :start,
+            stop: stop,
+            currentState:currentState
         }
 
     }
@@ -24,5 +35,10 @@ let obj = (function () {
     };
 })();
 
-//let obj1 = obj.getInstance();
-// let obj2 = obj.getInstance();
+let obj1 = obj.getInstance();
+let obj2 = obj.getInstance();
+console.log(obj1.currentState())
+console.log(obj2.currentState())
+obj1.start()
+console.log(obj1.currentState())
+console.log(obj2.currentState())
