6cfafa13a81a096157226983b2e952aac177e52c
Lucid <67615874+Lucid-017@users.noreply.github.com>
2022-05-24 17:05:41 +0100

    Question 6

---
+++ PrePopulated Arrays/PrepopulatedArray.js
@@ -0,0 +1,11 @@
+/**Create an array that is prepopulated
+ * With 5 random Numbers
+ */
+
+//version 1
+let a1 
+console.log('1', a1);
+
+//version 2
+let a2
+console.log('2', a2);
---
+++ PrePopulated Arrays/Question.md
@@ -0,0 +1,1 @@
+## create an array that is prepopulated with some sort of value
