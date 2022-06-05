86f25cc849b20e81678813e67032c08a4df8bf57
Lucid <67615874+Lucid-017@users.noreply.github.com>
2022-06-05 23:48:49 +0100

    solution

--- Object Props/ObjectProps.js
+++ Object Props/ObjectProps.js
@@ -13,4 +13,9 @@ let obj = {
     "b": 'fyra',
     c: 5
 };
-console.log(obj);
+console.log(obj);//{a:"tre",b:"fyra",c:5}
+// why?
+// this is because every prop defined in an object is read as a string by javascript
+// doesnt matter if they are in quotes or not,they are saved as string keys with assigned values
+// if u define repeat props like a,b and c ,your going to be left with whatever the final value for those
+// so these {a:"tre",b:"fyra",c:5} will overwrite whatever came b4 it
